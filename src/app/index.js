04eb2b9b04e7861e93245d4da77ce095526b7518
xr <1046210664@qq.com>
2023-07-26 23:42:45 +0800

    feat: 捕获错误返回

--- src/app/index.js
+++ src/app/index.js
@@ -5,11 +5,13 @@ const Koa = require("koa");
 const { koaBody } = require("koa-body");
 const static = require("koa-static");
 const views = require("koa-views");
+const error = require("koa-json-error");
 
 require("../db");
 const user = require("../routers/user");
 
 const app = new Koa();
+app.use(error());
 app.use(views(path.join(__dirname, "../views"), { map: { html: "ejs" } }));
 app.use(static(path.join(__dirname, "../public")));
 app.use(koaBody());
