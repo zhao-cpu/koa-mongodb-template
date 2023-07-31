1b2a03fe9a7f99855c443b3fd44ee741e0a37378
xr <1046210664@qq.com>
2023-07-31 20:46:21 +0800

    feat: async-validator 参数验证

--- src/controller/UserController.js
+++ src/controller/UserController.js
@@ -1,7 +1,47 @@
+const Schema = require("async-validator");
 const UserModel = require("../model/UserModel");
+const { phoneRegular } = require("../utils/regular");
+
 class UserController {
 	async create(ctx) {
 		const { body } = ctx.request;
+
+		const descriptor = {
+			userName: {
+				type: "string",
+				required: true,
+				message: "用户名必填",
+			},
+			password: {
+				type: "string",
+				required: true,
+				message: "密码必填",
+			},
+			phone: [
+				{
+					type: "number",
+					required: true,
+					message: "手机号必填",
+				},
+				{
+					validator: (rule, value) => {
+						const flag = phoneRegular.test(value);
+						return flag;
+					},
+					message: "请输入正确的手机号",
+				},
+			],
+			sex: {
+				type: "enum",
+				enum: [0, 1, 2],
+				required: true,
+				message: "性别必填",
+			},
+		};
+
+		const validator = new Schema.default(descriptor);
+		await validator.validate(body);
+
 		const data = await UserModel.create(body);
 		ctx.body = {
 			code: 0,
---
+++ src/utils/regular.js
@@ -0,0 +1,5 @@
+const phoneRegular = /^1[2-9]\d{9}$/;
+
+module.exports = {
+	phoneRegular,
+};
